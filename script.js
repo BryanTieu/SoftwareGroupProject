77e56d8850db2a3e7bcdf01b9d449ac54b76df34
Bryan Tieu <tieubryan1@gmail.com>
2021-08-04 03:50:58 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Get Quote Button Function for FuelQuote
 $(document).ready(function() {
 
     $('#formsubmit').click(function() {
@@ -8,6 +9,35 @@ $(document).ready(function() {
             }
         );
     });
-
 });
 
+// Disabling Submit and Get Quote Button with empty form values, better function.
+function success() {
+    if(document.getElementById("gallons").value==="" || document.getElementById("datepicker").value==="") {
+    document.getElementById('submit').disabled = true;
+    document.getElementById('formsubmit').disabled = true;
+    }
+    else {
+    document.getElementById('submit').disabled = false;
+    document.getElementById('formsubmit').disabled = false;
+    }
+}
+
+/*
+// Disabling Submit and Get Quote Button with empty form values, slight bug with delivery date
+const submitButton = document.getElementById("submit");
+const getQuoteButton = document.getElementById("formsubmit");
+[document.querySelector('#datepicker'),document.querySelector('#gallons')].forEach(item => {
+    item.addEventListener('keyup', (e) => {
+        const value = e.currentTarget.value;
+
+        submitButton.disabled = value === "";
+    });
+    item.addEventListener('keyup', (e) => {
+        const value = e.currentTarget.value;
+
+        getQuoteButton.disabled = value === "";
+    });
+});
+
+ */
